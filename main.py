b4cd98a7021323b6aa34ca75d725454a95b5c0a2
oken <nabilzulkefli72@gmail.com>
2018-02-28 23:52:06 +0800

    Audio processing

---
+++ main.py
@@ -0,0 +1,13 @@
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+
+y, sr = librosa.load("C:/Users/oken72/Desktop/Dev/lisa/data/timit/raw/TIMIT/TRAIN/DR1/FCJF0/SA1.WAV",
+                     res_type='kaiser_best')
+
+plt.figure(figsize=(20, 16))
+D = librosa.amplitude_to_db(librosa.stft(y), ref=np.max)
+plt.subplot(4, 2, 1)
+librosa.display.specshow(D, hop_length=512, y_axis='linear')
+plt.colorbar(format='%+2.0f dB')
+plt.title('Linear-frequency power spectogram')
